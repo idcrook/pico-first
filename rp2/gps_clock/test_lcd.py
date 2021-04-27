f630fd05c62ca52b603e858ea0cabd441aadcd36
David Crook <idcrook@users.noreply.github.com>
2021-04-27 17:16:43 -0600

    update test_lcd code
    - scan for LCD device

--- rp2/gps_clock/test_lcd.py
+++ rp2/gps_clock/test_lcd.py
@@ -8,23 +8,22 @@ import lcd
 from lcd.rp2_i2c_adafruit_lcd import I2cLcd
 
 # The MCP23008 has a jumper selectable address: 0x20 - 0x27
-DEFAULT_I2C_ADDR = 0x20
+LCD_I2C_ADDR = 0x20
+LCD_AVAILABLE = False
 
 I2C_BUS_NUMBER = 0
 I2C_SDA = Pin(8)
 I2C_SCL = Pin(9)
 I2C_FREQUENCY = 400000
 
-def test_main():
-    """Test function for verifying basic functionality."""
-    print("Running test_main")
-    i2c = I2C(I2C_BUS_NUMBER, sda=I2C_SDA, scl=I2C_SCL, freq=I2C_FREQUENCY)
-    lcd = I2cLcd(i2c, DEFAULT_I2C_ADDR, 2, 16)
+def test_lcd(i2c, lcd_addr):
+    print("Running test_lcd")
+    lcd = I2cLcd(i2c, lcd_addr, 2, 16)
     lcd.putstr("It Works!\nSecond Line")
     utime.sleep_ms(3000)
     lcd.clear()
     count = 0
-    while True:
+    while count < 11:
         lcd.move_to(0, 0)
         lcd.putstr("%7d" % (utime.ticks_ms() // 1_000))
         utime.sleep_ms(1000)
@@ -49,5 +48,22 @@ def test_main():
             print("Turning display & backlight on")
             lcd.backlight_on()
             lcd.display_on()
+    lcd.backlight_off()
+    lcd.display_off()
+
+def test_main():
+    """Test function for verifying basic functionality."""
+    global LCD_AVAILABLE
+    print("Running test_main")
+    i2c = I2C(I2C_BUS_NUMBER, sda=I2C_SDA, scl=I2C_SCL, freq=I2C_FREQUENCY)
+    device_addresses = i2c.scan()
+    if LCD_I2C_ADDR in device_addresses:
+        LCD_AVAILABLE = True
+
+    if LCD_AVAILABLE:
+        test_lcd(i2c, LCD_I2C_ADDR)
+    else:
+        print("LCD device not found")
+ 
 
 test_main()
